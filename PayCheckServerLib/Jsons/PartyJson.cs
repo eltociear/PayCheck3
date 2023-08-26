a83e266c577a93b55d51ae880ad63e51df3c324e
HW12Dev <26612968+HW12Dev@users.noreply.github.com>
2023-08-26 21:32:07 +1000

    Add experimental party support

---
+++ PayCheckServerLib/Jsons/PartyJson.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+namespace PayCheckServerLib.Jsons {
+    public class PartyJoinCodePostBody
+    {
+        [JsonProperty("code")]
+        public string Code { get; set; }
+    }
+    public class PartyPatchJsonBody
+    {
+        [JsonProperty("attributes")]
+        public PartyJson.DataJson.AttributesJson Attributes { get; set; }
+        [JsonProperty("inactiveTimeout")]
+        public int InactiveTimeout { get; set; }
+        [JsonProperty("inviteTimeout")]
+        public int InviteTimeout { get; set; }
+        [JsonProperty("minPlayers")]
+        public int MinPlayers { get; set; }
+    }
+    public class PartyPostJsonBody {
+		public partial class MembersJson
+        {
+			[JsonProperty("iD")]
+			public string Id { get; set; }
+			[JsonProperty("statusV2")]
+			public string StatusV2 { get; set; }
+			[JsonProperty("platformID")]
+			public string PlatformId { get; set; }
+			[JsonProperty("platformUserID")]
+			public string PlatformUserId { get; set; }
+		}
+		[JsonProperty("attributes")]
+        public PartyJson.DataJson.AttributesJson Attributes { get; set; }
+        [JsonProperty("configurationName")]
+        public string ConfigurationName { get; set; }
+        [JsonProperty("members")]
+        public List<MembersJson> Members { get; set; }
+    }
+	public class PartyJson
+        {
+        public partial class PagingJson
+            {
+            [JsonProperty("first")]
+            public string First { get; set; }
+            [JsonProperty("last")]
+            public string Last { get; set; }
+            [JsonProperty("next")]
+            public string Next { get; set; }
+            [JsonProperty("previous")]
+            public string Previous { get; set; }
+        }
+        public partial class ConfigurationJson
+        {
+            public partial class NativeSessionSettingJson
+            {
+                public partial class LocalizedSessionNameJson
+                {
+                    public partial class LocalizedTextJson {
+                        [JsonProperty("de-DE")]
+                        public string DeDE { get; set; }
+                        [JsonProperty("en-US")]
+                        public string EnUS { get; set; }
+                        [JsonProperty("es-419")]
+                        public string Es419 { get; set; }
+                        [JsonProperty("es-ES")]
+                        public string EsES { get; set; }
+                        [JsonProperty("fr-FR")]
+                        public string FrFR { get; set; }
+                        [JsonProperty("it-IT")]
+                        public string ItIT { get; set; }
+                        [JsonProperty("ja-JP")]
+                        public string JaJP { get; set; }
+                        [JsonProperty("ko-KR")]
+                        public string KoKR { get; set; }
+                        [JsonProperty("pl-PL")]
+                        public string PlPL { get; set; }
+                        [JsonProperty("pt-BR")]
+                        public string PtBR { get; set; }
+                        [JsonProperty("ru-RU")]
+                        public string RuRU { get; set; }
+                        [JsonProperty("tr-TR")]
+                        public string TrTR { get; set; }
+                        [JsonProperty("zh-Hans")]
+                        public string ZhHans { get; set; }
+                        [JsonProperty("zh-Hant")]
+                        public string ZhHant { get; set; }
+                    }
+                    [JsonProperty("defaultLanguage")]
+                    public string DefaultLanguage { get; set; }
+                    [JsonProperty("localizedText")]
+                    public LocalizedTextJson LocalizedText { get; set; }
+                }
+                [JsonProperty("XboxSessionTemplateName")]
+                public string XboxSessionTemplateName { get; set; }
+                [JsonProperty("XboxServiceConfigID")]
+                public string XboxServiceConfigId { get; set; }
+                [JsonProperty("PSNServiceLabel")]
+                public int PSNServiceLabel { get; set; }
+                [JsonProperty("SessionTitle")]
+                public string SessionTitle { get; set; }
+                [JsonProperty("ShouldSync")]
+                public bool ShouldSync { get; set; }
+                [JsonProperty("PSNSupportedPlatforms")]
+                // might be another type
+                public List<string> PSNSupportedPlatforms { get; set; }
+                [JsonProperty("localizedSessionName")]
+                public LocalizedSessionNameJson LocalizedSessionName { get; set; }
+            }
+            [JsonProperty("persistent")]
+            public bool Persistent { get; set; }
+            [JsonProperty("textChat")]
+            public bool TextChat { get; set; }
+            [JsonProperty("autoJoin")]
+            public bool AutoJoin { get; set; }
+            [JsonProperty("tieTeamsSessionLifetime")]
+            public bool TieTeamsSessionLifetime { get; set; }
+            [JsonProperty("minPlayers")]
+            public int MinPlayers { get; set; }
+            [JsonProperty("maxPlayers")]
+            public int MaxPlayers { get; set; }
+            [JsonProperty("inviteTimeout")]
+            public int InviteTimeout { get; set; }
+            [JsonProperty("inactiveTimeout")]
+            public int InactiveTimeout { get; set; }
+            [JsonProperty("name")]
+            public string Name { get; set; }
+            [JsonProperty("type")]
+            public string Type { get; set; }
+            [JsonProperty("joinability")]
+            public string Joinability { get; set; }
+            [JsonProperty("deployment")]
+            public string Deployment { get; set; }
+            [JsonProperty("clientVersion")]
+            public string ClientVersion { get; set; }
+            [JsonProperty("requestedRegions")]
+            public List<string> RequestedRegions { get; set; }
+            [JsonProperty("dsSource")]
+            public string DsSource { get; set; }
+            [JsonProperty("preferredClaimKeys")]
+            public object PreferredClaimKeys { get; set; }
+            [JsonProperty("fallbackClaimKeys")]
+            public object FallbackClaimKeys { get; set; }
+            [JsonProperty("nativeSessionSetting")]
+            public NativeSessionSettingJson NativeSessionSetting { get; set; }
+            [JsonProperty("PSNBaseURL")]
+            public string PSNBaseURL { get; set; }
+        };
+        public partial class DataJson
+        {
+            public partial class MembersJson
+            {
+                [JsonProperty("id")]
+                public string Id { get; set; }
+                [JsonProperty("status")]
+                public string Status { get; set; }
+                [JsonProperty("statusV2")]
+                public string StatusV2 { get; set; }
+                [JsonProperty("updatedAt")]
+                public string UpdatedAt { get; set; }
+                [JsonProperty("platformID")]
+                public string PlatformId { get; set; }
+                [JsonProperty("platformUserID")]
+                public string PlatformUserId { get; set; }
+            }
+            public partial class AttributesJson {
+                public partial class AttributesPreferenceJson
+                {
+                    [JsonProperty("crossplay_enabled")]
+                    public bool CrossplayEnabled { get; set; }
+                    [JsonProperty("current_platform")]
+                    public string CurrentPlatform { get; set; }
+                }
+                [JsonProperty("DIFFICULTYIDX")]
+                public int DifficultyIdx { get; set; }
+                [JsonProperty("LEVELIDX")]
+                public int LevelIdx { get; set; }
+                [JsonProperty("PLAYERSNUM")]
+                public int PlayersNum { get; set; }
+                [JsonProperty("PORTBEACON")]
+                public int PortBeacon { get; set; }
+                [JsonProperty("PORTGAME")]
+                public int PortGame { get; set; }
+                [JsonProperty("SECURITYCOMPANIES")]
+                public string SecurityCompanies { get; set; }
+                [JsonProperty("SESSIONTEMPLATENAME")]
+                public string SessionTemplateName { get; set; }
+                [JsonProperty("STATUS")]
+                public string Status { get; set; }
+                [JsonProperty("TEXTCHAT")]
+                public bool TextChat { get; set; }
+                [JsonProperty("preference")]
+                public AttributesPreferenceJson Preference { get; set; }
+            }
+            [JsonProperty("code")]
+            public string Code { get; set; }
+            [JsonProperty("isActive")]
+            public bool IsActive { get; set; }
+            [JsonProperty("isFull")]
+            public bool IsFull { get; set; }
+            [JsonProperty("version")]
+            public int Version { get; set; }
+            [JsonProperty("id")]
+            public string Id { get; set; }
+            [JsonProperty("namespace")]
+            public string Namespace { get; set; }
+            [JsonProperty("createdAt")]
+            public string CreatedAt { get; set; }
+            [JsonProperty("createdBy")]
+            public string CreatedBy { get; set; }
+            [JsonProperty("updatedAt")]
+            public string UpdatedAt { get; set; }
+            [JsonProperty("leaderID")]
+            public string LeaderId { get; set; }
+            [JsonProperty("configuration")]
+            public ConfigurationJson Configuration { get; set; }
+            [JsonProperty("members")]
+            public List<MembersJson> Members { get; set; }
+            [JsonProperty("attributes")]
+            public AttributesJson Attributes { get; set; }
+        }
+        [JsonProperty("paging")]
+        public PagingJson Paging { get; set; }
+        [JsonProperty("data")]
+        public List<DataJson> Data { get; set; }
+    }
+}
---
+++ PayCheckServerLib/Responses/Party.cs
@@ -0,0 +1,241 @@
+﻿using NetCoreServer;
+using Newtonsoft.Json;
+using PayCheckServerLib.Jsons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PayCheckServerLib.Responses
+	{
+	public class Party {
+		// indexed by 6 character code
+		static Dictionary<string, PartyJson.DataJson> ActiveParties = new();
+
+		static PartyJson.DataJson CreateParty(string userid, PartyPostJsonBody body) {
+			var partycode = new string(Enumerable.Repeat("ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", 6).Select(str => str[new Random().Next(str.Length)]).ToArray());
+			PartyJson.DataJson party = new() {
+				Code = partycode,
+				IsActive = true,
+				IsFull = false,
+				Version = 5,
+				Id = Guid.NewGuid().ToString().Replace("-", ""),
+				Namespace = "pd3beta",
+				CreatedAt = DateTime.Now.ToUniversalTime().ToString("o"),
+				CreatedBy = userid,
+				UpdatedAt = DateTime.Now.ToUniversalTime().ToString("o"),
+				LeaderId = userid,
+				Configuration = new() {
+					Persistent = false,
+					TextChat = true,
+					AutoJoin = false,
+					TieTeamsSessionLifetime = false,
+					MinPlayers = 1,
+					MaxPlayers = 4,
+					InviteTimeout = 60,
+					InactiveTimeout = 60,
+					Name = body.ConfigurationName,
+					Type = "NONE",
+					// others might be: PUBLIC, PRIVATE, FRIENDS_ONLY
+					Joinability = "INVITE_ONLY",
+					Deployment = "",
+					ClientVersion = "0",
+					RequestedRegions = new() {"eu-central-1","eu-north-1","eu-west-1"},
+					DsSource = "",
+					PreferredClaimKeys = null,
+					FallbackClaimKeys = null,
+					NativeSessionSetting = new() {
+						XboxSessionTemplateName = "Payday3",
+						XboxServiceConfigId = "00000000-0000-0000-0000-00006056e5d6",
+						PSNServiceLabel = 0,
+						SessionTitle = "Payday3 Party",
+						ShouldSync = true,
+						PSNSupportedPlatforms = new() {},
+						LocalizedSessionName = new() {
+							DefaultLanguage = "en-US",
+							LocalizedText = new() {
+								DeDE = "GRUPPE Payday3",
+								EnUS = "Payday3 Party",
+								Es419 = "GRUPO Payday3",
+								EsES = "GRUPO Payday3",
+								FrFR = "GROUPE Payday3",
+								ItIT = "SQUADRA Payday3",
+								JaJP = "Payday3 パーティー",
+								KoKR = "Payday3 GRUP",
+								PlPL = "ZESPÓŁ Payday3",
+								PtBR = "GRUPO Payday3",
+								RuRU = "КОМАНДА Payday3",
+								TrTR = "Payday3 파티",
+								ZhHans = "Payday3 参加者",
+								ZhHant = "Payday3 隊伍"
+							}
+						}
+					},
+					PSNBaseURL = "https://s2s.sp-int.playstation.net"
+				},
+				Members = new() {
+					new() {
+						Id = userid,
+						Status = "JOINED",
+						StatusV2 = "JOINED",
+						UpdatedAt = DateTime.Now.ToUniversalTime().ToString("o"),
+						PlatformId = body.Members[0].PlatformId,
+						PlatformUserId = body.Members[0].PlatformUserId
+					}
+				},
+				Attributes = new() {
+					DifficultyIdx = 0,
+					LevelIdx = -1,
+					PlayersNum = 1,
+					PortBeacon = 0,
+					PortGame = 0,
+					SecurityCompanies = "0",
+					SessionTemplateName = body.Attributes.SessionTemplateName,
+					Status = "Idle",
+					TextChat = true,
+					Preference = new() {
+						CrossplayEnabled = true,
+						CurrentPlatform = body.Members[0].PlatformId
+					}
+				}
+			};
+
+			Debugger.PrintInfo(String.Format("Party created with code: {0}", partycode));
+
+			return party;
+		}
+
+		static List<PartyJson.DataJson> PartiesPlayerIsIn(string userid) {
+			List<PartyJson.DataJson> ret = new();
+			foreach (var pair in ActiveParties) {
+				var party2 = pair.Value;
+				foreach (var member in party2.Members) {
+					if (member.Id == userid) {
+						ret.Add(party2);
+					}
+				}
+			}
+			return ret;
+		}
+
+		static PartyJson.DataJson GetPartyById(string id) {
+			foreach (var party in ActiveParties) {
+				if(party.Value.Id == id) {
+					return party.Value;
+				}
+			}
+			return null;
+		}
+
+		[HTTP("GET", "/session/v1/public/namespaces/pd3beta/users/me/parties")]
+		public static bool SessionsParties(HttpRequest request, PC3Server.PC3Session session) {
+			// change this to be dynamic asap
+			var userid = "29475976933497845197035744456968";
+
+			ResponseCreator response = new ResponseCreator();
+
+			List<PartyJson.DataJson> partiesPlayerIsIn = PartiesPlayerIsIn(userid);
+
+			PartyJson party = new() {
+				Data = partiesPlayerIsIn,
+				Paging = new() {
+					First = "",
+					Last = "",
+					Next = "",
+					Previous = ""
+				}
+			};
+
+			response.SetBody(JsonConvert.SerializeObject(party));
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+
+		[HTTP("POST", "/session/v1/public/namespaces/pd3beta/party")]
+		public static bool PostParty(HttpRequest request, PC3Server.PC3Session session) {
+			// change this to be dynamic asap
+			var userid = "29475976933497845197035744456968";
+			var body = JsonConvert.DeserializeObject<PartyPostJsonBody>(request.Body);
+
+			ResponseCreator response = new ResponseCreator();
+
+			var party = CreateParty(userid, body);
+
+			ActiveParties.Add(party.Code, party);
+
+			response.SetBody(JsonConvert.SerializeObject(party));
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+
+		[HTTP("DELETE", "/session/v1/public/namespaces/pd3beta/parties/{partyid}/users/me/leave")]
+		public static bool LeaveParty(HttpRequest request, PC3Server.PC3Session session) {
+			// change this to be dynamic asap
+			var userid = "29475976933497845197035744456968";
+			var partyid = session.HttpParam["partyid"];
+
+			var party = GetPartyById(partyid);
+
+			List<PartyJson.DataJson.MembersJson> newmembers = new();
+			foreach (var member in party.Members) {
+				if(member.Id != userid) {
+					newmembers.Add(member);
+				}
+			}
+			party.Members = newmembers;
+
+			ActiveParties[party.Code] = party;
+
+			ResponseCreator response = new ResponseCreator(204);
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+
+		[HTTP("PATCH", "/session/v1/public/namespaces/pd3beta/parties/{partyid}")]
+		public static bool PatchParty(HttpRequest request, PC3Server.PC3Session session) {
+			var newparty = JsonConvert.DeserializeObject<PartyPatchJsonBody>(request.Body);
+
+			if(newparty == null) {
+				ResponseCreator response2 = new ResponseCreator(400);
+				session.SendResponse(response2.GetResponse());
+				return true;
+			}
+
+			var partyid = session.HttpParam["partyid"];
+			var party = GetPartyById(partyid);
+			party.Attributes = newparty.Attributes;
+			
+			if(newparty.InactiveTimeout != 0) {
+				party.Configuration.InactiveTimeout = newparty.InactiveTimeout;
+			}
+			if(newparty.InviteTimeout != 0) {
+				party.Configuration.InviteTimeout = newparty.InviteTimeout;
+			}
+			if(newparty.MinPlayers != 0) {
+				party.Configuration.MinPlayers = newparty.MinPlayers;
+			}
+			ActiveParties[party.Code] = party;
+
+			ResponseCreator response = new ResponseCreator();
+			response.SetBody(JsonConvert.SerializeObject(party));
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+
+		// todo: use slejmur's userid branch and add player to party
+		[HTTP("POST", "/session/v1/public/namespaces/pd3beta/parties/users/me/join/code")]
+		public static bool JoinPartyByCode(HttpRequest request, PC3Server.PC3Session session) {
+			var body = JsonConvert.DeserializeObject<PartyJoinCodePostBody>(request.Body);
+
+			if (ActiveParties[body.Code] == null) {
+				return true;
+			}
+
+			ResponseCreator response = new ResponseCreator();
+			response.SetBody(JsonConvert.SerializeObject(ActiveParties[body.Code]));
+			session.SendResponse(response.GetResponse());
+			return true;
+		}
+	}
+}
